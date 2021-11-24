70a7592dbbdda4013b5f6503d6362c4df3b1927c
Marcelo Valdivia <72950694@ucsm.edu.pe>
2021-11-24 17:29:05 -0500

    feat: Dashboard components

--- src/views/Dashboard.tsx
+++ src/views/Dashboard.tsx
@@ -1,7 +1,46 @@
-import React from 'react'
+/* eslint-disable no-console */
+import React, {useState, useEffect} from 'react'
+import styled from 'styled-components'
+import {gql, useQuery} from '@apollo/client'
+import DashboardCard from '../components/DashboardCard'
+
+const Grid = styled.div`
+  display: flex;
+  flex-direction: row;
+  margin-top: 32px;
+`
+
+const GridColum = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  padding: 0;
+  align-items: center;
+`
 
 const Dashboard = () => {
-  return <div>DASHBOARD</div>
+  //Fetch status to build the columns
+  const GET_STATUS = gql`
+    query getTasks {
+      tasks(input: {}) {
+        id
+        name
+        owner {
+          id
+          fullName
+        }
+      }
+    }
+  `
+  const {loading, error, data} = useQuery(GET_STATUS)
+  console.log(data)
+  return (
+    <Grid>
+      <GridColum>
+        <DashboardCard />
+      </GridColum>
+    </Grid>
+  )
 }
 
 export default Dashboard
