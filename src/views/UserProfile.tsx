d2157275bd30e70d38b7462a3e5c13b14245f93f
Marcelo Valdivia <72950694@ucsm.edu.pe>
2021-11-25 16:28:00 -0500

    feat: User Profile done

--- src/views/UserProfile.tsx
+++ src/views/UserProfile.tsx
@@ -1,5 +1,22 @@
+/* eslint-disable no-console */
 import React from 'react'
-import styled from 'styled-components'
+import styled, {css} from 'styled-components'
+import {gql, useQuery} from '@apollo/client'
+import Spinner from '../components/Spinner/Spinner'
+
+interface AvatarProps {
+  image: string | null
+}
+
+const baseTextStyles = css`
+  font-style: normal;
+  font-size: 16px;
+  line-height: 24px;
+  font-weight: 600;
+  letter-spacing: 0.75px;
+  color: #ffffff;
+  justify-self: flex-end;
+`
 
 const ProfileContainer = styled.div`
   display: flex;
@@ -13,13 +30,12 @@ const CardProfile = styled.div`
   display: flex;
   flex-direction: column;
   min-width: 32rem;
-  min-height: 24rem;
+  min-height: 32rem;
   background: #2c2f33;
-
   border-radius: 16px;
   align-items: center;
   justify-content: center;
-  gap: 16px;
+  gap: 24px;
 `
 const UserPrimaryData = styled.div`
   display: flex;
@@ -30,58 +46,54 @@ const UserPrimaryData = styled.div`
   align-items: center;
 `
 
-const Avatar = styled.div`
-  height: 40px;
-  width: 40px;
-  background: white;
+const Avatar = styled.div<AvatarProps>`
+  height: 64px;
+  width: 64px;
+  background: url(${props => props.image});
+  background-repeat: no-repeat;
+  background-size: cover;
   border-radius: 50%;
 `
 const TextHeader = styled.span`
-  font-style: normal;
-  font-size: 16px;
-  line-height: 24px;
-  font-weight: 600;
-  letter-spacing: 0.75px;
-  color: #ffffff;
-  justify-self: flex-end;
+  ${baseTextStyles};
+  text-transform: capitalize;
 `
 const TextContent = styled.span`
-  font-style: normal;
-  font-size: 16px;
-  line-height: 24px;
-  letter-spacing: 0.75px;
-  color: #ffffff;
-  justify-self: flex-end;
+  ${baseTextStyles};
+  font-weight: 100;
 `
 
 const UserProfile = () => {
+  const GET_USER = gql`
+    query getUser {
+      profile {
+        avatar
+        fullName
+        email
+        type
+        createdAt
+        updatedAt
+      }
+    }
+  `
+  const {loading, data, error} = useQuery(GET_USER)
+
+  if (loading) return <Spinner />
+  if (error) throw new Error(`Error! ${error.message}`)
   return (
     <ProfileContainer>
       <CardProfile>
-        <UserPrimaryData>
-          <Avatar />
-          <TextContent>NAME</TextContent>
-        </UserPrimaryData>
-        <UserPrimaryData>
-          <TextHeader>Email</TextHeader>
-          <TextContent>dsasda@ravn.co</TextContent>
-        </UserPrimaryData>
-        <UserPrimaryData>
-          <TextHeader>Type</TextHeader>
-          <TextContent>dsasda@ravn.co</TextContent>
-        </UserPrimaryData>
-        <UserPrimaryData>
-          <TextHeader>Position</TextHeader>
-          <TextContent>dsasda@ravn.co</TextContent>
-        </UserPrimaryData>
-        <UserPrimaryData>
-          <TextHeader>CreatedAt</TextHeader>
-          <TextContent>dsasda@ravn.co</TextContent>
-        </UserPrimaryData>
-        <UserPrimaryData>
-          <TextHeader>UpdatedAt</TextHeader>
-          <TextContent>dsasda@ravn.co</TextContent>
-        </UserPrimaryData>
+        {!data.profile.avatar && (
+          <Avatar image="https://avatars.dicebear.com/api/initials/mv.svg" />
+        )}
+        {Object.keys(data.profile)
+          .filter(key => key !== '__typename' && key !== 'avatar')
+          .map((key, index) => (
+            <UserPrimaryData key={index}>
+              <TextHeader>{key}</TextHeader>
+              <TextContent>{data.profile[key]}</TextContent>
+            </UserPrimaryData>
+          ))}
       </CardProfile>
     </ProfileContainer>
   )
