5e4dc61c305ecf62a3742f8c2cafd1fea26381db
Marcelo Valdivia <72950694@ucsm.edu.pe>
2021-11-26 22:19:57 -0500

    feat: creation modal advance

--- src/components/ActionBar.tsx
+++ src/components/ActionBar.tsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import styled from 'styled-components'
-import {Link, useMatch, useLocation} from 'react-router-dom'
+import {Link, useMatch} from 'react-router-dom'
 import * as Dialog from '@radix-ui/react-dialog'
-// import * as DropdownMenu from '@radix-ui/react-dropdown-menu'
-import {RiAddLine, RiFunctionLine, RiMenuLine} from 'react-icons/ri'
+import * as Dropdown from '@radix-ui/react-dropdown-menu'
+import {
+  RiAddLine,
+  RiFunctionLine,
+  RiIncreaseDecreaseFill,
+  RiMenuLine,
+  RiUser3Fill,
+  RiPriceTag3Fill,
+} from 'react-icons/ri'
 
 //Principal Container
 const ActionbarContainer = styled.div`
@@ -39,9 +46,15 @@ const Button = styled.button`
 `
 
 //Dialog
+const DialogContainer = styled(Dialog.Root)`
+  padding: 16px;
+`
+
 const DialogContent = styled(Dialog.Content)`
+  display: flex;
+  flex-direction: column;
+  gap: 24px;
   width: 431px;
-  height: 184px;
   padding: 16px;
   background-color: #393d41;
   border-radius: 8px;
@@ -50,9 +63,7 @@ const DialogContent = styled(Dialog.Content)`
   position: fixed;
   top: 50%;
   left: 50%;
-  transform: translate(-50%, -50%);
-
-  padding: 25;
+  transform: translate(-50%, -156%);
 `
 const StyledOverlay = styled(Dialog.Overlay)`
   background: rgba(0, 0, 0, 0.15);
@@ -77,19 +88,93 @@ const TaskNameInput = styled.input`
     outline: none;
   }
 `
-const DataContainer = styled.div`
+const DropdownContainer = styled.div`
   display: flex;
   display: row;
   justify-content: space-between;
   width: 100%;
+  gap: 16px;
 `
 const ModalButtonsContainer = styled.div`
   display: flex;
   flex-direction: row;
   justify-content: flex-end;
   width: 100%;
-  height: 24px;
+  height: 40px;
   gap: 16px;
+`
+
+//Estimated Points Dropdown
+const EstimatedPointsDropdown = styled(Dropdown.Content)`
+  display: flex;
+  margin-top: 12px;
+  width: 132px;
+  flex-direction: column;
+  gap: 10px;
+  border: 1px solid #94979a;
+  padding-bottom: 8px;
+  background: #393d41;
+  box-sizing: border-box;
+  border-radius: 8px;
+  z-index: 1;
+  box-shadow: 0px 10px 38px -10px rgba(22, 23, 24, 0.35),
+    0px 10px 20px -15px rgba(22, 23, 24, 0.2);
+`
+
+const TriggerDropdown = styled(Dropdown.Trigger)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  width: 128px;
+  max-height: 32px;
+  gap: 8px;
+  padding: 0 12px;
+  background: rgba(148, 151, 154, 0.1);
+  border-radius: 4px;
+  cursor: pointer;
+  appearance: none;
+  border-style: none;
+`
+
+const TriggerLabel = styled.span`
+  font-weight: 600;
+  font-size: 15px;
+  line-height: 24px;
+  letter-spacing: 0.75px;
+  color: #ffffff;
+`
+const EstimatedPointsItemHeader = styled(Dropdown.Item)`
+  display: flex;
+  flex-direction: row;
+  margin-left: 16px;
+  margin-top: 8px;
+  font-style: normal;
+  font-weight: 600;
+  font-size: 20px;
+  line-height: 32px;
+  letter-spacing: 0.75px;
+  color: #94979a;
+`
+const EstimatedPointsItem = styled(Dropdown.Item)`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+  margin-left: 16px;
+  margin-right: 16px;
+  border-radius: 4px;
+  cursor: pointer;
+  ::focus {
+    background: #94979a;
+  }
+`
+const EstimatedPointsItemLabel = styled.span`
+  font-style: normal;
+  font-weight: normal;
+  font-size: 15px;
+  line-height: 24px;
+  letter-spacing: 0.75px;
+  color: #ffffff;
 `
 
 //Modal Buttons
@@ -123,26 +208,24 @@ const CreateButton = styled.button`
 `
 
 const ActionBar = () => {
-  const location = useLocation()
   const match = useMatch(location.pathname)
+  // const [estimatedPoints, setEstimatedPoints] = useState(-1)
+  // const [users, setUsers] = useState([])
+  // const [selectedUser, setSelectedUser] = useState({})
+  // const [tags, setTags] = useState([])
 
+  const points = [
+    {id: 1, value: 0},
+    {id: 2, value: 1},
+    {id: 3, value: 2},
+    {id: 4, value: 4},
+    {id: 5, value: 8},
+  ]
   return (
     <ActionbarContainer>
       <SwitchContainer>
-        <Link to="/">
-          <RiMenuLine
-            style={{
-              height: '24px',
-              width: '24px',
-              color: match?.pathname === '/' ? '#da584b' : '#94979A',
-              border: match?.pathname === '/' ? '1px solid #da584b' : 'none',
-              borderRadius: '8px',
-              padding: '11px',
-            }}
-          />
-        </Link>
         <Link to="/my-tasks">
-          <RiFunctionLine
+          <RiMenuLine
             style={{
               height: '24px',
               width: '24px',
@@ -154,9 +237,21 @@ const ActionBar = () => {
             }}
           />
         </Link>
+        <Link to="/">
+          <RiFunctionLine
+            style={{
+              height: '24px',
+              width: '24px',
+              color: match?.pathname === '/' ? '#da584b' : '#94979A',
+              border: match?.pathname === '/' ? '1px solid #da584b' : 'none',
+              borderRadius: '8px',
+              padding: '11px',
+            }}
+          />
+        </Link>
       </SwitchContainer>
       <form>
-        <Dialog.Root>
+        <DialogContainer>
           <Dialog.Trigger asChild>
             <Button>
               <RiAddLine style={{height: '24px', width: '24px'}} />
@@ -164,10 +259,50 @@ const ActionBar = () => {
           </Dialog.Trigger>
           <StyledOverlay />
           <DialogContent>
-            <Dialog.Title>
+            <Dialog.Title style={{margin: 0}}>
               <TaskNameInput placeholder="Task Tittle" />
             </Dialog.Title>
-            <DataContainer></DataContainer>
+            <DropdownContainer>
+              <Dropdown.Root>
+                <TriggerDropdown>
+                  <RiIncreaseDecreaseFill
+                    style={{width: '32px', height: '24px', color: 'white'}}
+                  />
+                  <TriggerLabel>Estimate</TriggerLabel>
+                </TriggerDropdown>
+                <EstimatedPointsDropdown>
+                  <EstimatedPointsItemHeader>
+                    Estimate
+                  </EstimatedPointsItemHeader>
+                  {points.map(point => (
+                    <EstimatedPointsItem key={point.id}>
+                      <RiIncreaseDecreaseFill
+                        style={{width: '32px', height: '26px', color: 'white'}}
+                      />
+                      <EstimatedPointsItemLabel>
+                        {point.value} Points
+                      </EstimatedPointsItemLabel>
+                    </EstimatedPointsItem>
+                  ))}
+                </EstimatedPointsDropdown>
+              </Dropdown.Root>
+              <Dropdown.Root>
+                <TriggerDropdown>
+                  <RiUser3Fill
+                    style={{width: '32px', height: '32px', color: 'white'}}
+                  />
+                  <TriggerLabel>Assignee</TriggerLabel>
+                </TriggerDropdown>
+              </Dropdown.Root>
+              <Dropdown.Root>
+                <TriggerDropdown>
+                  <RiPriceTag3Fill
+                    style={{width: '24px', height: '32px', color: 'white'}}
+                  />
+                  <TriggerLabel>Label</TriggerLabel>
+                </TriggerDropdown>
+              </Dropdown.Root>
+            </DropdownContainer>
             <ModalButtonsContainer>
               <Dialog.Close asChild>
                 <CancelButton>Cancel</CancelButton>
@@ -177,7 +312,7 @@ const ActionBar = () => {
               </Dialog.Close>
             </ModalButtonsContainer>
           </DialogContent>
-        </Dialog.Root>
+        </DialogContainer>
       </form>
     </ActionbarContainer>
   )
