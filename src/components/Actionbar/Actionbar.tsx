0dc58bb83448bd0ae9f06af85545278ee8a51839
Marcelo Valdivia <72950694@ucsm.edu.pe>
2021-12-09 10:59:27 -0500

    fix: unnecessary fragments removed

--- src/components/Actionbar/Actionbar.tsx
+++ src/components/Actionbar/Actionbar.tsx
@@ -2,7 +2,7 @@ import React, {useState} from 'react'
 import {Link, useMatch} from 'react-router-dom'
 import {RiAddLine, RiFunctionLine, RiMenuLine} from 'react-icons/ri'
 import 'react-toastify/dist/ReactToastify.css'
-import ModalCreate from '../Modal/ModalCreate/ModalCreate'
+import CreateTaskModal from '../Modal/CreateTaskModal/CreateTaskModal'
 import {
   ActionbarContainer,
   Button,
@@ -14,48 +14,43 @@ const Actionbar = () => {
   const [openModal, setOpenModal] = useState(false)
 
   return (
-    <>
-      <ActionbarContainer>
-        <SwitchContainer>
-          <Link to="/my-tasks">
-            <RiMenuLine
-              style={{
-                height: '16px',
-                width: '16px',
-                color: match?.pathname === '/my-tasks' ? '#da584b' : '#94979A',
-                border:
-                  match?.pathname === '/my-tasks'
-                    ? '1px solid #da584b'
-                    : 'none',
-                borderRadius: '8px',
-                padding: '11px',
-              }}
-            />
-          </Link>
-          <Link to="/">
-            <RiFunctionLine
-              style={{
-                height: '16px',
-                width: '16px',
-                color: match?.pathname === '/' ? '#da584b' : '#94979A',
-                border: match?.pathname === '/' ? '1px solid #da584b' : 'none',
-                borderRadius: '8px',
-                padding: '11px',
-              }}
-            />
-          </Link>
-        </SwitchContainer>
-        <>
-          <ModalCreate
-            show={openModal}
-            onClick={() => setOpenModal(!openModal)}
+    <ActionbarContainer>
+      <SwitchContainer>
+        <Link to="/my-tasks">
+          <RiMenuLine
+            style={{
+              height: '16px',
+              width: '16px',
+              color: match?.pathname === '/my-tasks' ? '#da584b' : '#94979A',
+              border:
+                match?.pathname === '/my-tasks' ? '1px solid #da584b' : 'none',
+              borderRadius: '8px',
+              padding: '11px',
+            }}
           />
-          <Button onClick={() => setOpenModal(!openModal)}>
-            <RiAddLine style={{height: '20px', width: '20px'}} />
-          </Button>
-        </>
-      </ActionbarContainer>
-    </>
+        </Link>
+        <Link to="/">
+          <RiFunctionLine
+            style={{
+              height: '16px',
+              width: '16px',
+              color: match?.pathname === '/' ? '#da584b' : '#94979A',
+              border: match?.pathname === '/' ? '1px solid #da584b' : 'none',
+              borderRadius: '8px',
+              padding: '11px',
+            }}
+          />
+        </Link>
+      </SwitchContainer>
+
+      <CreateTaskModal
+        show={openModal}
+        onClick={() => setOpenModal(!openModal)}
+      />
+      <Button onClick={() => setOpenModal(!openModal)}>
+        <RiAddLine style={{height: '20px', width: '20px'}} />
+      </Button>
+    </ActionbarContainer>
   )
 }
 
