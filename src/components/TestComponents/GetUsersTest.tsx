36b4094e24195b0ab893332113fe3c3c5899057b
Marcelo Valdivia <72950694@ucsm.edu.pe>
2021-12-15 10:38:50 -0500

    feat: getUsers test added

---
+++ src/components/TestComponents/GetUsersTest.tsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {useQuery} from '@apollo/client'
+import {GET_USERS} from '../../graphql/queries/queries'
+
+interface User {
+  __typename: string
+  id: string
+  avatar: string
+  fullName: string
+}
+interface TaskProps {
+  assignee: User
+}
+
+const GetUsersTest = () => {
+  const {loading, error: getUsersError, data} = useQuery(GET_USERS)
+
+  if (loading) return <div>Loading...</div>
+  if (getUsersError) return <div>Oh no!</div>
+
+  return (
+    <div>
+      {data.tasks.map((task: TaskProps) => (
+        <li key={task.assignee.id}>{task.assignee.fullName}</li>
+      ))}
+    </div>
+  )
+}
+
+export default GetUsersTest
